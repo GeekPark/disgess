8d1279e90473c1750b271bbc1fa98cb33667c5a4
Vanilla <mail@liyaodong.com>
2016-03-21 14:56:31 +0800

    fix lint error

--- src/example.js
+++ src/example.js
@@ -7,6 +7,6 @@ const token = 'dcca26991894c75acee49c631cdb0c8ead8d511e84debcb216472336e1acd983'
 const loginURL = '//dev.geekpark.net:3000/user/login';
 
 ReactDOM.render(
-  <Comments isAdmin id={214790} type="Topic" {...{token, loginURL}} />,
+  <Comments isAdmin id={214790} type="Topic" {...{ token, loginURL }} />,
   document.querySelector('#app')
 );
